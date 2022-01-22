d6ccae2e4ad560cc392c9c009a72ccc5073f72fa
Roger Curley <rocurley@gmail.com>
2022-01-22 16:14:56 -0500

    Add iai benchmark

---
+++ benches/day12_iai.rs
@@ -0,0 +1,15 @@
+use aoc2021::day12::{parse, solve_inner};
+use iai::black_box;
+
+const input: [&'static str; 23] = [
+    "yw-MN", "wn-XB", "DG-dc", "MN-wn", "yw-DG", "start-dc", "start-ah", "MN-start", "fi-yw",
+    "XB-fi", "wn-ah", "MN-ah", "MN-dc", "end-yw", "fi-end", "th-fi", "end-XB", "dc-XB", "yw-XN",
+    "wn-yw", "dc-ah", "MN-fi", "wn-DG",
+];
+
+pub fn benchmark() -> (usize, usize) {
+    let (start, end, edges) = parse(black_box(&input));
+    solve_inner(start, end, &edges)
+}
+
+iai::main!(benchmark);
