2ec99012f08838ac5353e2e0c747723ae0ec9405
Roger Curley <rocurley@gmail.com>
2022-02-13 15:54:04 -0500

    Better benchmarking

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,9 +1,5 @@
 use aoc2021::{find_paths_and_join, find_small_loops, parse, solve};
-use cpuprofiler::PROFILER;
-use criterion::profiler::Profiler;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::path::Path;
-use std::sync::MutexGuard;
 
 const input: [&'static str; 23] = [
     "yw-MN", "wn-XB", "DG-dc", "MN-wn", "yw-DG", "start-dc", "start-ah", "MN-start", "fi-yw",
@@ -42,23 +38,5 @@ targets = criterion_benchmark
 criterion_main!(benches);
 
 fn profiled() -> Criterion {
-    Criterion::default().with_profiler(CPUProfiler { profiler: None })
-}
-
-struct CPUProfiler<'a> {
-    profiler: Option<MutexGuard<'a, cpuprofiler::Profiler>>,
-}
-
-impl<'a> Profiler for CPUProfiler<'a> {
-    fn start_profiling(&mut self, benchmark_id: &str, benchmark_dir: &Path) {
-        let mut profiler = PROFILER.lock().unwrap();
-        profiler
-            .start(format!("profiling/{}", benchmark_id))
-            .unwrap();
-        self.profiler = Some(profiler);
-    }
-    fn stop_profiling(&mut self, benchmark_id: &str, benchmark_dir: &Path) {
-        let mut p = self.profiler.take().unwrap();
-        p.stop().unwrap();
-    }
+    Criterion::default()
 }
